a2512937f33507682f4d0b7f4f6517701e635c57
Shivam Sherkar <shivam23sherkar@gmail.com>
2021-10-19 12:52:35 +0530

    added provisioner folder

---
+++ section-3/provisioner.tf
@@ -0,0 +1,44 @@
+#provisioners are used to execute scripts on local or remote  machine as part of resource creation
+
+resource "aws_instance" "myec2" {
+  ami                    = "ami-02e136e904f3da870"
+  instance_type          = "t2.micro"
+  key_name               = "terraform"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras install -y nginx1.12",
+      "sudo systemctl start nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("./terraform.pem")
+      host        = self.public_ip
+    }
+  }
+}
+
+### security group resource to allow the terraform provisioner from laptop to connect to EC2 Instance via SSH.
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
