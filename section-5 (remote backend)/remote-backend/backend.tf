9e9e197031e891aa8ec6cdc1f5ea7e3b352306c4
Shivam Sherkar <shivam23sherkar@gmail.com>
2021-10-22 14:27:04 +0530

    added remote backend

---
+++ section-5 (remote backend)/remote-backend/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {
+    bucket = "shivam-remote-backend"
+    key    = "remotedemo.tfstate"   #name of the state file which we need
+    region = "us-east-1"
+    dynamodb_table = "s3-state-lock"
+  }
+}
