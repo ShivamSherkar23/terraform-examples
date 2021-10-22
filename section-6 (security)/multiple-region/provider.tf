d3cada6f418f153d2afcbdcf219de0e1147b5b98
Shivam Sherkar <shivam23sherkar@gmail.com>
2021-10-22 15:39:44 +0530

    added multiple region config

---
+++ section-6 (security)/multiple-region/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     =  "us-east-1"
+  shared_credentials_file = "C:\\Users\\Shivam Sherkar\\.aws\\credentials"
+  profile                 = "default"
+}
+
+provider "aws" {
+  alias      =  "aws02"
+  region     =  "ap-south-1"
+  shared_credentials_file = "C:\\Users\\Shivam Sherkar\\.aws\\credentials"
+  profile                 = "default"
+}
