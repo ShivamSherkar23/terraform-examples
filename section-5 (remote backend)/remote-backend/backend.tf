bd6c33fc71a889bd584f05b3eec1d030edeea268
Shivam Sherkar <shivam23sherkar@gmail.com>
2021-10-22 14:41:42 +0530

    change in the backend

--- section-5 (remote backend)/remote-backend/backend.tf
+++ section-5 (remote backend)/remote-backend/backend.tf
@@ -4,6 +4,6 @@ terraform {
     bucket = "shivam-remote-backend"
     key    = "remotedemo.tfstate"   #name of the state file which we need
     region = "us-east-1"
-    dynamodb_table = "s3-state-lock"
+    dynamodb_table = "s3-state-lock" #dynamodb table for the lock file entries
   }
 }
